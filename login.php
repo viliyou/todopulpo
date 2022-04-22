04b7d1dae03ff4e576c5c8618afe811dc236fea3
viliyou <92851775+viliyou@users.noreply.github.com>
2022-04-22 22:33:01 +0100

    Update login.php
    Corrijo un error del código del formulario. Enlazaba a login.php y lo correcto sería que enlazase a do_login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
   </head>
   <body>
     <h1>Login</h1>
-    <form action="login.php" method="post">
+    <form action="do_login.php" method="post">
       <input name="f_email" type="text" placeholder="e-mail" /><br />
       <input name="f_password" type="password" placeholder="Contraseña" /><br />
       <input type="submit" value="Login" /><br />
