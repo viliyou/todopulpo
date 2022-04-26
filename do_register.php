52fa60c00e08c12ecba346d72ae0be28af7d72fa
viliyou <92851775+viliyou@users.noreply.github.com>
2022-04-26 17:13:32 +0200

    Segundo punto del sprint #2
    Subo el archivo do_register.php que inserta un usuario en la base de datos (con contraseña hasheada) y redirige a la página login.php

---
+++ do_register.php
@@ -0,0 +1,18 @@
+<?php
+$db = mysqli_connect('localhost', 'root', '1234', 'todopulpo') or die('Fail');
+$name_posted = $_POST['f_name'];
+$email_posted = $_POST['f_email'];
+$password_posted = $_POST['f_password'];
+$password2_posted = $_POST['f_password2'];
+if ($password_posted != $password2_posted) {
+echo '<p>Introduce la misma contraseña en los dos campos</p>';
+echo '<p><a href='register.html'>Volver a intentarlo</a></p>';
+} else {
+$password_posted = password_hash($password_posted, PASSWORD_DEFAULT);
+$query = "INSERT INTO usuario(id_usuario,nombre, email, password)
+VALUES ('".$nombre_posted."',".$email_posted.",NULL)";
+mysqli_query($db, $query) or die('Error');
+echo '<p>Usuario registrado</p>'
+echo '<p><a href="login.html">Loguearse</p>'
+}
+?>
