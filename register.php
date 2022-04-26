5de1e2711f8d76cddef3cef25ac46a80435350cb
viliyou <92851775+viliyou@users.noreply.github.com>
2022-04-26 17:11:20 +0200

    Primer punto del sprint #2
    Subo la página register.php con un formulario con nombre, email, contraseña, repetir contraseña y un botón de registrar. El formulario se enlaza con un POST a do_register.php

---
+++ register.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <meta charset="UTF-8" />
+  </head>
+  <body>
+    <h1>Registro</h1>
+    <form action="do_register.php" method="post">
+      <input name="f_name" type="text" placeholder="nombre" /><br />
+      <input name="f_email" type="text" placeholder="e-mail" /><br />
+      <input name="f_password" type="password" placeholder="Contraseña" /><br />
+      <input name="f_password2" type="password" placeholder="Repite la contraseña" /><br />
+      <input type="submit" value="Regístrate" /><br />
+      
+    </form>
+  </body>
+</html>
