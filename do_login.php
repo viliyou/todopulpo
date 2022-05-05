e2cde670160411183fd28737c7ed90db23cc5b2e
viliyou <92851775+viliyou@users.noreply.github.com>
2022-05-05 12:47:54 +0200

    Update do_login.php

--- do_login.php
+++ do_login.php
@@ -1,17 +1,27 @@
 <?php
 $db = mysqli_connect('localhost', 'root', '1234', 'todopulpodb') or die('Fail');
+
 $email_posted = $_POST['f_email'];
 $password_posted = $_POST['f_password'];
+
 $query = "SELECT id, password FROM tUsuario WHERE email = '".$email_posted."'";
 $result = mysqli_query($db, $query) or die('Query error');
-if (mysqli_num_rows($result) > 0) {
+
+if (mysqli_num_rows($result) > 0) {  
 $only_row = mysqli_fetch_array($result);
+  
+  echo("fallo vaina linea 13");
+  
 if ($only_row[1] == $password_posted) {
-session_start();
+  
+  echo("fallo vaina linea 17");
+session_start();  
 $_SESSION['user_id'] = $only_row[0];
 header('Location: main.php');
+  
 } else {
 echo '<p>Contraseña incorrecta</p>';
+  
 }
 } else {
 echo '<p>Usuario no encontrado con ese email</p>';
