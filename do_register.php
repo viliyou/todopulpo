75c4ba2281e0f42c9733a58a331732de47dbaebc
viliyou <92851775+viliyou@users.noreply.github.com>
2022-05-05 11:56:59 +0200

    Update do_register.php
    probando

--- do_register.php
+++ do_register.php
@@ -8,10 +8,19 @@ $email_posted = $_POST['f_email'];
 $password_posted = $_POST['f_password'];
 $password2_posted = $_POST['f_password2'];
 
+echo ($name_posted);
+echo ($email_posted);
+echo ($password_posted);
+echo ($password2_posted);
+
+  
+
 if ($password_posted != $password2_posted) {
 echo '<p>Introduce la misma contraseña en los dos campos</p>';
 echo '<p><a href='register.php'>Volver a intentarlo</a></p>';
+  
 } else {
+echo ("cualquier vaina");
 $password_posted = password_hash($password_posted, PASSWORD_DEFAULT);
 $query = "INSERT INTO tUsuario(nombre, email, password)
 VALUES (.$name_posted.",".$email_posted.",.$password_posted.)";
