db96aa38f5ed760d18cc60b938f20a04b414bffc
viliyou <92851775+viliyou@users.noreply.github.com>
2022-05-06 11:45:33 +0200

    Update main.ph
    Añado el if para comprobar si está logeado o no el usuario

--- main.php
+++ main.php
@@ -1,14 +1,29 @@
-<!DOCTYPE html>
+!DOCTYPE html>
+
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Todopulpo</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>    
+    <body> 
     <h1>Página principal</h1>
-    <a href="login.php">Login</a>
-    
-</body>
+    <?php        
+    session_start();        
+    if (empty($_SESSION['user_id'])) {         
+    ?>
+        <div class="contenedorLogin">
+            <p>No estás logueado <a href='/login.php'> Iniciar Sesión</a>.</p>
+        </div>
+    <?php
+    } else {
+    ?>
+        <div class="main">
+            <p>Estás logueado</p>
+        </div>        
+     <?php
+           }
+     ?>        
+    </body>
 </html>
