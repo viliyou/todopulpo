7cb3a6d41e2fe13c49cd0d5335878f31dcd651f7
viliyou <92851775+viliyou@users.noreply.github.com>
2022-05-26 12:23:00 +0200

    Update main.php

--- main.php
+++ main.php
@@ -67,9 +67,9 @@
         while($registro2 = mysqli_fetch_array($result2)){
 
             $notamedia = $notamedia + $registro2[0];
-           // $comentario = $comentario + "/n" + $registro2[1];
+           $comentario = $comentario + "/n" + $registro2[1];
             echo ($registro2[1]);
-            $comentario = $registro2[1];
+           // $comentario = $registro2[1];
             $division = $division + 1;
             
         }
