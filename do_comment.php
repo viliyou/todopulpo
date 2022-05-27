dce4d5f441a6009e8c8eb6e9cf36a60bdff575b3
viliyou <92851775+viliyou@users.noreply.github.com>
2022-05-27 11:51:54 +0200

    Add files via upload

---
+++ do_comment.php
@@ -0,0 +1,27 @@
+<?php
+//ini_set('display_errors', 'On');
+//require __DIR__ . 'db_connection.php';
+//$db = get_db_connection_or_die();
+
+$db = mysqli_connect('localhost', 'root', '1234', 'todopulpodb') or die('Fail');
+
+$nota_posted = $_POST['f_nota'];
+$comentario_posted = $_POST['f_comentario'];
+
+
+echo ($nota_posted);
+echo ($comentario_posted);
+
+
+  
+
+
+$query = "INSERT INTO tValoracion(nota, comentario) VALUES ('".$nota_posted."','".$comentario_posted."')";
+mysqli_query($db, $query) or die('Error');
+//echo '<p>Usuario registrado</p>';
+//echo '<p><a href="login.php">Loguearse</p>';
+
+
+header("Location: main.php");
+
+?>
