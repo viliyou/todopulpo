5cdf67f527202a4774f5cc949df1c785f6690927
viliyou <92851775+viliyou@users.noreply.github.com>
2022-05-31 11:48:12 +0200

    Update do_login.php

--- do_login.php
+++ do_login.php
@@ -10,7 +10,7 @@ $result = mysqli_query($db, $query) or die('Query error');
 if (mysqli_num_rows($result) > 0) {  
 $only_row = mysqli_fetch_array($result);
   
-  echo("fallo vaina linea 13");
+ 
   
   echo($only_row[1]);
   
@@ -18,7 +18,7 @@ $only_row = mysqli_fetch_array($result);
   
 if (password_verify($password_posted, $only_row[1])) {
   
-  echo("fallo vaina linea 17");
+  
 session_start();  
 $_SESSION['user_id'] = $only_row[0];
 header('Location: main.php');
