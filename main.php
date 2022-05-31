2fb7e34f5323bfce70ed8c628327a44182bc3960
viliyou <92851775+viliyou@users.noreply.github.com>
2022-05-31 12:13:12 +0200

    Update main.php

--- main.php
+++ main.php
@@ -96,11 +96,11 @@
         
          echo "   <tr> ";
         
-         echo "  <form action="do_comment.php" method="post">";
+         echo "  <form action='do_comment.php' method='post'>";
             
-         echo "  <input name="f_nota" type="number" placeholder="nota" />";      
-         echo "  <input name="f_comentario" type="text" placeholder="comentario" />";
-         echo "  <input type="submit" value="Enviar" />  "; 
+         echo "  <input name='f_nota' type='number' placeholder='nota' />";      
+         echo "  <input name='f_comentario' type='text' placeholder='comentario' />";
+         echo "  <input type='submit' value='Enviar' />  "; 
          echo"   </form> ";
             
          echo "   </tr>";
